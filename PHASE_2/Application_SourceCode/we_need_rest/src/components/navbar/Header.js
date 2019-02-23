4b65b0fc0ca31ffb55aa95911d857ba657a4f918
Nabil Shaikh <Nabil.Shaikh.Nabil@gmail.com>
2019-02-23 17:23:30 +1100

    fixed navbar again

--- PHASE_2/Application_SourceCode/we_need_rest/src/components/navbar/Header.js
+++ PHASE_2/Application_SourceCode/we_need_rest/src/components/navbar/Header.js
@@ -6,18 +6,38 @@ class Header extends Component {
     constructor(props) {
         super(props);
     }
+    
+    componentDidMount() {
+        let url = window.location.href;
+        
+        console.log(url);
+        if (url.search("about") != -1) {
+            this.activate(document.getElementById('about'));
+        } else if (url.search("contact") != -1) {
+            this.activate(document.getElementById('contact'));
+        } else {
+            this.activate(document.getElementById('home'));
+        }
+    }
 
     handleClick(elem) {
-        let oldActive = document.getElementsByClassName("active")[0];
-        oldActive.classList.remove("active");
+        this.deactivate( document.getElementsByClassName("active")[0] );
+        this.activate(elem);
+    }
+
+    activate(elem) {
         elem.classList.add("active");
+    }
+
+    deactivate(elem) {
+        elem.classList.remove("active");
     }
 
     render() {
       return (
         <div className="nav">
             <ul>
-                <li className="active" id="home" onClick={()=>this.handleClick(document.getElementById('home'))}>
+                <li id="home" onClick={()=>this.handleClick(document.getElementById('home'))}>
                     <Link to='/'>Home</Link>
                 </li>
                 <li id="about" onClick={()=>this.handleClick(document.getElementById('about'))}>
