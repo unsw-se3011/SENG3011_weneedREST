ddef128416b96404cdb8b87ce148bf4d8953f366
Nabil Shaikh <Nabil.Shaikh.Nabil@gmail.com>
2019-02-23 17:09:05 +1100

    tarot cards v2

--- PHASE_2/Application_SourceCode/we_need_rest/src/components/contact/contact.js
+++ PHASE_2/Application_SourceCode/we_need_rest/src/components/contact/contact.js
@@ -22,7 +22,7 @@ class Contact extends Component {
           <Card name="Bailey"   url="https://i.pinimg.com/736x/42/cd/84/42cd84625ba3ed7f52c5bb81a612486d--tarot-card-art-tarot-cards.jpg"/>
           <Card name="Nabil"    url="http://flavorwire.files.wordpress.com/2013/08/tarot__justice_by_sceithailm-d659u191.jpg"/>
           <Card name="Estella"  url="https://i.pinimg.com/736x/a4/28/c2/a428c29a8c484778140d8dea23203500--tarot-card-art-tarot-cards-art-illustration.jpg"/>
-          <Card name="Jacob"    url="https://i.pinimg.com/236x/e0/6d/e3/e06de30b8d61f2826ef7532ca8265888.jpg"/>
+          <Card name="Jacob"    url="http://geekologie.com/2013/08/23/lotr-tarot-cards-5.jpg"/>
           <Card name="Harry"    url="https://i.pinimg.com/originals/41/d5/53/41d5538ed2d66ebc13424ec8e95c56d8.jpg"/>
         </div>
       </Margin>
