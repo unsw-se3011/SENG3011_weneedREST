f676fe1104e028fe51786575285e339e6d9df840
Bailey Ivancic <bailey_ivancic@outlook.com>
2019-02-23 12:54:04 +1100

    Changed robot.txt settings in settings.py. Now scraping WHO website

--- PHASE_1/API_SourceCode/webScraper/webScraper/spiders/spider.py
+++ PHASE_1/API_SourceCode/webScraper/webScraper/spiders/spider.py
@@ -7,12 +7,12 @@ class Spider(scrapy.Spider):
     name="spider"
 
     def start_requests(self):
-        urls=['https://www.who.int/emergencies/diseases/en/']
+        urls = ['https://www.who.int/emergencies/diseases/en/']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        page = 1
         filename = 'WHO-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
