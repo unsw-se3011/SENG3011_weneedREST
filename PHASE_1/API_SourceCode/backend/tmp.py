69c2619ee0b2a52078860456e56581df6ae226de
Bailey Ivancic <bailey_ivancic@outlook.com>
2019-03-21 16:55:31 +1100

    Updated requirements.txt + changing over to curl

--- PHASE_1/API_SourceCode/backend/tmp.py
+++ PHASE_1/API_SourceCode/backend/tmp.py
@@ -1,9 +1,10 @@
 import re
+import requests
+import pycurl
 
-with open('GIM-1.html',encoding = "ISO-8859-1") as f:
-	m = re.search(r'addMarker\(\{.*\);', f.read(), re.MULTILINE|re.DOTALL)
-	if m:
-		print(m[0])
+c = pycurl.Curl()
+
+
 
 with open('GIM-1.html',encoding = "ISO-8859-1") as f:
 	m2 = re.search(r'var incidents = \[.*\]', f.read(), re.MULTILINE|re.DOTALL)
