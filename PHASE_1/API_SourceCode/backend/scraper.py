24a4283170a6e4022d8e14ad471c710d5081adc0
Bailey Ivancic <bailey_ivancic@outlook.com>
2019-03-22 00:52:46 +1100

    Removed redundant scrapy files from repo

--- PHASE_1/API_SourceCode/backend/scraper.py
+++ PHASE_1/API_SourceCode/backend/scraper.py
@@ -4,19 +4,19 @@ import pycurl
 import io
 from io import StringIO
 
-
-with open('GIM-1.html', 'wb') as f:
+# Pycurl scrapes raw HTML and saves to 
+with open('GIM-1.html', 'wb') as g:
     c = pycurl.Curl()
     c.setopt(c.URL, 'http://outbreaks.globalincidentmap.com/')
-    c.setopt(c.WRITEDATA, f)
+    c.setopt(c.WRITEDATA, g)
     c.perform()
     c.close()
+    g.close()
 
-# Commented out for testing, sorry Harry - Bailey
-# with open('GIM-1.html',encoding = "ISO-8859-1") as f:
-# 	m2 = re.search(r'var incidents = \[.*\]', f.read(), re.MULTILINE|re.DOTALL)
-# 	if m2:
-# 		print(m2[0])
+with open('GIM-1.html',encoding = "ISO-8859-1") as f:
+	m2 = re.search(r'var incidents = \[.*\]', f.read(), re.MULTILINE|re.DOTALL)
+	if m2:
+		print(m2[0])
 
 
 f.closed
