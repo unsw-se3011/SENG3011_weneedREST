571746d649fd3eb6ddccd0e4f398152b3f3d0ed6
Bailey Ivancic <bailey_ivancic@outlook.com>
2019-03-22 11:31:42 +1100

    Cleaning

--- PHASE_1/API_SourceCode/backend/routes.py
+++ PHASE_1/API_SourceCode/backend/routes.py
@@ -41,23 +41,27 @@ class hello(Resource):
 
 # Returns all reports 
 @api.route('/allReports')
+@api.response(200: 'Success')
 class allReports(Resource):
     def get(self):
         return response, 200
 
 # Returns reports specifying selected criteria
 @api.route('/reports')
+@api.response()
 @api.doc(params={'n': 'Number of results returned', 'location':'Geocode of area affected', 'key_terms':'Comma separated list of of all key items requested by user', 'date':'Date in either date_exact or date_range format', 'date_exact':'yyyy-mm-ddThh:mm:ss. Year field mandatory, every other segment optional', 'date_range':'d1 to d2 with d1 being an exact date before d2'})
 class specificReports(Resource):
     @api.doc(responses={'200':'Successful', '400':'Invalid Location, Key Term or Date'})
     def get(self, n, location, key_terms, date):
-        return response, 400
+        return response
 
 # Deletes a report
 @api.route('/delete')
 @api.doc(params={'id': 'ID of report to be deleted'})
 class delete(Resource):
-    @api.doc(responses={'200': 'Successful', '400':'Invalid ID', '404': 'Report not found'})
+    @api.response(200: 'Success')
+    @api.response(400: 'Invalid ID')
+    @api.response(404: 'Report not found')
     def delete(self, id):
         return "deleted"
 
@@ -65,7 +69,10 @@ class delete(Resource):
 @api.route('/postReport')
 @api.doc(params={'id': 'ID of report to be posted'})
 class postReport(Resource):
-    @api.doc(responses={'200': 'Successful', '400':'Invalid ID', '404': 'Report not found', '405': 'Invalid data'})
+    @api.response(200: 'Success')
+    @api.response(400: 'Invalid ID')
+    @api.response(404: 'Report not found')
+    @api.response(405: 'Invalid data')
     def post(self, id):
         return "posted"
 
