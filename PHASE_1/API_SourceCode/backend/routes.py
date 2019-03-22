0a63e446a096867fc219f1d259a7784653c18b5e
Nabil Shaikh <Nabil.Shaikh.Nabil@gmail.com>
2019-03-22 13:06:15 +1100

    parsing arguments

--- PHASE_1/API_SourceCode/backend/routes.py
+++ PHASE_1/API_SourceCode/backend/routes.py
@@ -1,6 +1,6 @@
 from server import app
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -32,7 +32,6 @@ response ={
         ]
     }
 
-
 # Testing
 @api.route('/hello')
 class hello(Resource):
@@ -45,13 +44,21 @@ class allReports(Resource):
     def get(self):
         return response, 200
 
+parser = reqparse.RequestParser()
+
+parser.add_argument('n', type=int, required=True, help='n - number of results', location='headers')
+parser.add_argument('location', type=str, required=True, help='location of reports', location='headers')
+parser.add_argument('key_terms', required=True, help='list of key terms', location='headers')
+parser.add_argument('date', required=True, help='date range of reports', location='headers')
 # Returns reports specifying selected criteria
 @api.route('/reports')
 @api.doc(params={'n': 'Number of results returned', 'location':'Geocode of area affected', 'key_terms':'Comma separated list of of all key items requested by user', 'date':'Date in either date_exact or date_range format', 'date_exact':'yyyy-mm-ddThh:mm:ss. Year field mandatory, every other segment optional', 'date_range':'d1 to d2 with d1 being an exact date before d2'})
 class specificReports(Resource):
     @api.doc(responses={'200':'Successful', '400':'Invalid Location, Key Term or Date'})
-    def get(self, n, location, key_terms, date):
-        return response, 400
+    def get(self):
+        args = parser.parse_args()
+        print(args)
+        return response, 200
 
 # Deletes a report
 @api.route('/delete')
@@ -59,20 +66,25 @@ class specificReports(Resource):
 class delete(Resource):
     @api.doc(responses={'200': 'Successful', '400':'Invalid ID', '404': 'Report not found'})
     def delete(self, id):
-        return reports
+        return response
+
 
 # Updates an existing report with form data
+# TODO - create arguments to fill in the response
 @api.route('/postReport')
-@api.doc(params={'id': 'ID of report to be posted'})
+@api.doc(params={'id': 'ID number of report'})
 class postReport(Resource):
     @api.doc(responses={'200': 'Successful', '400':'Invalid ID', '404': 'Report not found', '405': 'Invalid data'})
-    def post(self, id):
-        return reports
+    def post(self):
+        return response
 
 # Updates an existing report
-@api.route('/updateReport')
-@api.doc(params={'id': 'ID of report'})
+# TODO - create arguments to fill in the response
+@api.route('/updateReport<string:id>')
 class updateReport(Resource):
     @api.doc(responses={'200': 'Successful', '400':'Invalid ID', '404': 'Report not found', '405': 'Invalid data'})
-    def updateReport(self, id):
-        return reports
+    def post(self, id):
+        return response
+
+# PUT is for updating
+# Post is for creating
