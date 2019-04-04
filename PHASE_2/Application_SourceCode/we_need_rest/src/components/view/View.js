65bad4644286cb002b626edc8957e3dfb3924f51
Yakqob <jawahib@gmail.com>
2019-04-04 17:42:42 +1100

    Extracted data from input boxes

--- PHASE_2/Application_SourceCode/we_need_rest/src/components/view/View.js
+++ PHASE_2/Application_SourceCode/we_need_rest/src/components/view/View.js
@@ -2,19 +2,88 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 
 class View extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+          n : '',
+          latitude : '',
+          longitude : '',
+          key_terms : '',
+          start_date : '',
+          end_date : ''
+        }
+        
+        this.updateN = this.updateN.bind(this);
+        this.updateLatitude = this.updateLatitude.bind(this);
+        this.updateLongitude = this.updateLongitude.bind(this);
+        this.updateKeyTerms = this.updateKeyTerms.bind(this);
+        this.updateStartDate = this.updateStartDate.bind(this);
+        this.updateEndDate = this.updateEndDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    updateN(event){
+        this.setState({ n : event.target.value})
+    }
+    updateLatitude(event){
+        this.setState({latitude : event.target.value})
+    }
+    updateLongitude(event){
+        this.setState({longitude : event.target.value})
+    }
+    updateKeyTerms(event){
+        this.setState({ key_terms : event.target.value})
+    }
+    updateStartDate(event){
+        this.setState({ start_date : event.target.value})
+    }
+    updateEndDate(event){
+        this.setState({ end_date : event.target.value})
+    }
+    onSubmit(e) {
+        console.log(this.state.n);
+        console.log(this.state.latitude);
+        console.log(this.state.longitude);
+        console.log(this.state.key_terms);
+        console.log(this.state.start_date);
+        console.log(this.state.end_date);
+
+    }
     render() {
         return (
-            // <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
             <div id="body">
                 <center>
                     <h1>View Reports</h1>
                 </center>
-                <div class="criteria">
-                        <p for="inputPassword4" class="text-dark">Keywords</p>
-                        {/* <input id = "keywords" value="{{keywords}}" type="text" name="title" class="form-control" id="inputPassword4" placeholder="Type in keywords"> */}
-                </div>
+                <form className="n">
+                    <div class="number">
+                            <p for="inputPassword4" class="text-dark">Number of Reports</p>
+                            <input id = "idn" name="title" onChange={this.updateN} type="text" class="form-control" placeholder="e.g. 7" />
+                    </div>
+                    <div class="lat">
+                            <p for="inputPassword4" class="text-dark">Latitude</p>
+                            <input id = "idLat" name="title" onChange={this.updateLatitude} type="text" class="form-control" placeholder="e.g. 211442" />
+                    </div>
+                    <div class="lon">
+                            <p for="inputPassword4" class="text-dark">Longitude</p>
+                            <input id = "idLon" name="title" onChange={this.updateLongitude} type="text" class="form-control" placeholder="e.g. 211442" />
+                    </div>
+                    <div class="terms">
+                            <p for="inputPassword4" class="text-dark">Key Terms</p>
+                            <input id = "idTerms" name="title" onChange={this.updateKeyTerms} type="text" class="form-control" placeholder="e.g. Malaria, Zika" />
+                    </div>
+                    <div class="start">
+                            <p for="inputPassword4" class="text-dark">From Date/Time:</p>
+                            <input id = "idStart" name="title" onChange={this.updateStartDate} type="text" class="form-control" placeholder="e.g. 2018-12-10T23:50:00" />
+                    </div>
+                    <div class="end">
+                            <p for="inputPassword4" class="text-dark">To Date/Time:</p>
+                            <input id = "idEnd" name="title" onChange={this.updateEndDate} type="text" class="form-control" placeholder="e.g. 2018-12-10T23:50:00" />
+                    </div>
+                </form>
+                <button type="button" onClick={this.onSubmit} className="button">Get</button>
             </div>
-            // {/* {n, latitude, longitude, key terms, start date, end date} */}
+            //{/* {n, latitude, longitude, key terms, start date, end date} */}
         )
     }
 }
