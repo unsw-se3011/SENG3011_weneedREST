08fb19c20e8f7e1e8049465cba3087eaea2c4345
Bailey Ivancic <bailey_ivancic@outlook.com>
2019-04-09 12:02:15 +1000

    Changed selected back to set to avoid duplicates

--- PHASE_2/Application_SourceCode/we_need_rest/src/components/home/Home.js
+++ PHASE_2/Application_SourceCode/we_need_rest/src/components/home/Home.js
@@ -86,7 +86,7 @@ class Home extends Component {
 
     this.state = {
       response: undefined,
-      selectedArticles: [],
+      selectedArticles: new Set,
       n : undefined,
       latitude : undefined,
       longitude : undefined,
@@ -122,7 +122,7 @@ class Home extends Component {
 
   select(report) {
     let temp = this.state.selectedArticles;
-    temp.push(report);
+    temp.add(report);
     this.setState({selectedArticles: temp});
     console.log(this.state.selectedArticles);
   }
