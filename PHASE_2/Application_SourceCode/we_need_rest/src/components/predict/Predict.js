b44b9d8287deeb5c24c92f386ca44a6304baa856
harryytang <z5116894@student.unsw.edu.au>
2019-04-09 23:05:30 +1000

    updated predict header, moved contact.css to correct spot

---
+++ PHASE_2/Application_SourceCode/we_need_rest/src/components/predict/Predict.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Predict extends Component {
+    render() {
+        return (
+            <div className="test">
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+  
+export default Predict;
